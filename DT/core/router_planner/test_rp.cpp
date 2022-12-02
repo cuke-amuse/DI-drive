aeff160bb53eabb437bff59483f6cdbd33cdee0b
cuke-amuse <56332297+cuke-amuse@users.noreply.github.com>
2022-12-02 22:46:19 +0800

    Add files via upload

---
+++ DT/core/router_planner/test_rp.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+TEST(RP , add)
+{
+    //Solution sl;
+    //vector<vector<int>> isConnected = {{1,0,0,1},{0,1,1,0},{0,1,1,1},{1,0,1,1}};
+    //sl.findCircleNum(isConnected);
+
+    //GTEST_ASSERT_EQ(add(10, 22), 32);
+}
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ DT/test_main.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+//#include "547.h"
+
+TEST(myfunctions, add)
+{
+    //Solution sl;
+    //vector<vector<int>> isConnected = {{1,0,0,1},{0,1,1,0},{0,1,1,1},{1,0,1,1}};
+    //sl.findCircleNum(isConnected);
+
+    //GTEST_ASSERT_EQ(add(10, 22), 32);
+}
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
